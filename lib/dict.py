b43efe7691cdc14aeb5f1b44f65715b6312b2f06
天海星夜 <knifemule@gmail.com>
2020-04-24 00:07:38 +0800

    mysterious aquaman No.2 desu.

---
+++ lib/dict.py
@@ -0,0 +1,7 @@
+aimi = {'name': '爱美', 'id': 'aimi_sound', 'color': 'red'}
+ayasa = {'name': '伊藤彩沙', 'id': 'ayasa_ito', 'color': 'purple'}
+rimi = {'name': '西本里美', 'id': 'Rimi_nsmt', 'color': 'pink'}
+sae = {'name': '大冢纱英', 'id': 'OSae1010', 'color': 'blue'}
+ayaka = {'name': '大桥彩香', 'id': 'AyakaOhashi', 'color': 'yellow'}
+
+PPP = [aimi, ayasa, rimi, sae, ayaka]
